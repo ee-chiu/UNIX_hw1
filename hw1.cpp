3c61a6fd176d370cc6eaeaa8a94e24482e6810c3
ee-chiu <80535163+ee-chiu@users.noreply.github.com>
2022-03-16 18:07:16 +0800

    get_command可正常return 但輸出還是怪

--- hw1.cpp
+++ hw1.cpp
@@ -4,22 +4,19 @@
 #include <dirent.h>
 #include <string.h>
 
-void get_command(const char *dir){
-    char cmdline_path[30];
+char* get_command(const char *dir){
+    char *cmdline_path = new char[30];
     strcpy(cmdline_path, dir);
     strcat(cmdline_path, "/cmdline");
     FILE *cmdline_file = fopen(cmdline_path, "r");
-    char cmdline_txt[1000];
+    char *cmdline_txt = new char[1000];
     fscanf(cmdline_file, "%s", cmdline_txt);
-    printf("%s\n", cmdline_txt);
-    return;
-    //return cmdline_txt;
+    return cmdline_txt;
 }
 
 void get_info(const char *dir){
-    get_command(dir);
-    //const char *cmdline_txt = get_command(dir);
-    //printf("%s\n", cmdline_txt);
+    char* cmdline_txt = get_command(dir);
+    printf("%s\n", cmdline_txt);
 }
 
 
@@ -31,7 +28,6 @@ int main(int argc, char** argv){
         if(isnum(dirp->d_name)) {
             char dir[20] = "/proc/";
             strcat(dir, dirp->d_name);
-            //printf("%s, ", dir);
             get_info(dir);            
             }
     }
