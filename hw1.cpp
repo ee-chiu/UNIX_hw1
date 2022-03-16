fb3256d0d1033a2e9fe705dc5d482e4e8a8bc51e
ee-chiu <80535163+ee-chiu@users.noreply.github.com>
2022-03-16 20:01:25 +0800

    command, pid done

--- hw1.cpp
+++ hw1.cpp
@@ -17,10 +17,10 @@ struct info{
 char* get_command(const char *dir){
     char *cmdline_path = new char[30];
     strcpy(cmdline_path, dir);
-    strcat(cmdline_path, "/cmdline");
+    strcat(cmdline_path, "/status");
     FILE *cmdline_file = fopen(cmdline_path, "r");
     char *cmdline_txt = new char[1000];
-    fscanf(cmdline_file, "%s", cmdline_txt);
+    fscanf(cmdline_file, "Name: %[^\n]", cmdline_txt);
     return cmdline_txt;
 }
 
